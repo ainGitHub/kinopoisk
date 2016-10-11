fb7bb6c7b394836599d9d6290ff7a03b57c4f114
Ainur <ainurmullin@mail.ru>
2016-10-11 23:31:35 +0300

    add UserService

---
+++ src/main/java/ru/dz/services/UserService.java
@@ -0,0 +1,23 @@
+package ru.dz.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.dz.entity.UserInfo;
+import ru.dz.repository.UserRepository;
+
+/**
+ * Created by ainur on 11.10.2016.
+ */
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public UserInfo getUser(Long id) {
+        return userRepository.findOne(id);
+    }
+
+    public void addUser(UserInfo user) {
+        userRepository.save(user);
+    }
+}
