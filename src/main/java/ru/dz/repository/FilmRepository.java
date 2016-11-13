98403cd88c5b54e1ef9e960f39e8c238bdcdfc36
Ainur <ainurmullin@mail.ru>
2016-11-13 11:37:30 +0300

    add film service and repository

---
+++ src/main/java/ru/dz/repository/FilmRepository.java
@@ -0,0 +1,21 @@
+package ru.dz.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import ru.dz.entity.Film;
+
+import java.util.List;
+
+/**
+ * Created by ainur on 10.11.2016.
+ */
+@Repository
+public interface FilmRepository extends JpaRepository<Film, Long> {
+    @Query("select f from Film f where f.deleted = true")
+    public List<Film> findAllDeleted();
+
+    @Query("select f from Film f where f.changed = true")
+    public List<Film> findAllChanged();
+}
---
+++ src/main/java/ru/dz/services/FilmService.java
@@ -0,0 +1,27 @@
+package ru.dz.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.dz.entity.Film;
+import ru.dz.repository.FilmRepository;
+
+import java.util.List;
+
+/**
+ * Created by ainur on 10.11.2016.
+ */
+@Service
+public class FilmService {
+
+    @Autowired
+    FilmRepository filmRepository;
+
+    public List<Film> findAllChangedFilms() {
+        return filmRepository.findAllChanged();
+    }
+
+
+    public List<Film> findAllDeletedFilms() {
+        return filmRepository.findAllDeleted();
+    }
+}
