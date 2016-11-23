250b9066bdca9e95faa927c8455ce79b3b74e3da
Ainur <ainurmullin@mail.ru>
2016-11-23 07:41:28 +0300

    change filmGenerateService, now it add films  to the elastic and db

--- src/main/java/ru/dz/services/FirstGenerateFilms.java
+++ src/main/java/ru/dz/services/FirstGenerateFilms.java
@@ -1,9 +1,12 @@
 package ru.dz.services;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Charsets;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.hibernate.annotations.common.util.impl.LoggerFactory;
+import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.dz.elastic.FilmSearchService;
 import ru.dz.entity.Film;
 import ru.dz.repository.FilmRepository;
 import com.google.common.io.Resources;
@@ -19,13 +22,21 @@ import java.util.List;
  */
 @Service
 public class FirstGenerateFilms {
+    boolean generated = false;
+    Logger logger = LoggerFactory.logger(FirstGenerateFilms.class);
 
     @Autowired
     FilmRepository filmRepository;
 
+    @Autowired
+    FilmSearchService filmSearchService;
+
     private List<Film> films = new ArrayList<>();
 
     public void generateFilms() {
+        if (generated)
+            return;
+
         String filmsStr = getResource("films.json");
         ObjectMapper objectMapper = new ObjectMapper();
         try {
@@ -36,10 +47,12 @@ public class FirstGenerateFilms {
         }
 
         initializeDBWithGeneratedFilms();
+        generated = true;
     }
 
     private void initializeDBWithGeneratedFilms() {
         filmRepository.save(films);
+        films.stream().forEach(f -> filmSearchService.add(f));
     }
 
     private String getResource(String file) {
