bef450680f41c237439a95f5b5938e561f15f110
Ainur <ainurmullin@mail.ru>
2016-12-08 18:21:08 +0300

    change view

--- src/main/resources/static/resources/index.js
+++ src/main/resources/static/resources/index.js
@@ -1,4 +1,85 @@
+var Film = React.createClass({
+    render() {
+        let film = this.props.film;
+        let link = "/film/" + film.id;
+        return (
+            <li>
+                <h2><a href={link}>{film.name}</a></h2>
+                <img src={film.image} width="180" height="280"/>
+                <p className="description">{film.description}</p>
+                <p><a className="more-link" href={link}>Продолжить<span className="icon">:</span></a></p>
+            </li>
+        );
+    }
+});
+
+var Search = React.createClass({
+    filterFilms() {
+        var name = this.refs.nameRef.value;
+        if (name != null) {
+            this.props.filterFilms(name);
+        }
+    },
+
+    render(){
+
+        return (
+            <aside role="complementary" className="container">
+                <h2>Поиск фильмов</h2>
+                <div className="input-group add-on">
+                    <input className="form-control" ref="nameRef" placeholder="Search" name="srch-term" id="srch-term"
+                           type="text"/>
+                    <div className="input-group-btn">
+                        <button className="btn btn-default" type="submit" onClick={this.filterFilms}>Поиск</button>
+                    </div>
+                </div>
+            </aside>
+        );
+    }
+});
+
+var App = React.createClass({
+    getInitialState: function () {
+        return {films: []};
+    },
+
+    componentWillMount() {
+        axios.get(`/all`)
+            .then(res => {
+                this.setState({films: res.data});
+            });
+    },
+
+
+    filterFilms (name){
+        axios.get(`/search/film?name=` + name)
+            .then(res => {
+                console.log(res.date);
+                this.setState({films: res.data});
+            });
+    },
+
+    render() {
+        self = this;
+
+        let films = this.state.films.map((film) => {
+            return <Film film={film} key={"/film/" + film.id}/>
+        });
+        return (
+            <div>
+                <Search filterFilms={this.filterFilms} key="search"/>
+                <article className="post content">
+                    <ul className="post-list">
+                        {films}
+                    </ul>
+                </article>
+            </div>
+        )
+    }
+});
+
+
 ReactDOM.render(
-    <App />,
+    <App/>,
     document.getElementById('root')
 );
--- src/main/resources/templates/films.ftl
+++ src/main/resources/templates/films.ftl
@@ -33,9 +33,9 @@
         </#if>
     </article>-->
 </section>
-<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 <script src="/resources/react/react.js"></script>
 <script src="/resources/react/react-dom.js"></script>
 <script src="/resources/react/browser.min.js"></script>
-<script type="text/babel" src="/resources/app.js"></script>
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+<script type="text/babel" src="/resources/index.js"></script>
 </#macro>
