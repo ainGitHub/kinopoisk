9e7561cc8127c1f32399e874e0bd2cf04ceb61ac
Ainur <ainurmullin@mail.ru>
2016-12-11 13:37:45 +0300

    add new search by genre method

--- src/main/resources/static/resources/index.js
+++ src/main/resources/static/resources/index.js
@@ -21,27 +21,65 @@ var Search = React.createClass({
         }
     },
 
-    autocomplete() {
+    search(event) {
         let nameRefInput = this.refs.nameRef;
 
         if (!nameRefInput.value.length) {
             return;
         }
+        /*  this.autocomplete(nameRefInput.value);*/
 
-        this.props.autocomplete(nameRefInput.value);
+        this.props.search(nameRefInput.value);
+    },
+
+    autocomplete(q) {
+        let nameRefInput = this.refs.nameRef;
+        let help = this.refs.helpInput;
+
+        if (!nameRefInput.value.length) {
+            return;
+        }
+
+        axios.get('/autocomplete', {params: {q: q}})
+            .then(res => {
+                if (res.data.length == 0)
+                    return;
+                let hits = res.data.hits.hits;
+                if (hits.length != 0) {
+                    console.log(hits[0].highlight.name);
+                    help.value = hits[0].highlight.name;
+                } else {
+                    nameRefInput.placeholder = "Search";
+                }
+            });
+    },
+
+    genreSearch(event) {
+        let selVal = this.refs.select.value;
+        this.props.filterByGenre(selVal);
     },
 
     render(){
+        let genres = this.props.genres.map(genre => {
+            return <option key={genre.id}>{genre.name}</option>;
+        });
 
         return (
             <aside role="complementary" className="container">
                 <h2>Поиск фильмов</h2>
                 <div className="input-group add-on">
                     <input className="form-control" ref="nameRef" placeholder="Search" name="srch-term" id="srch-term"
-                           type="text" onKeyDown={this.autocomplete}/>
+                           type="text" onKeyDown={this.search}/>
                     <div className="input-group-btn">
                         <button className="btn btn-default" type="submit" onClick={this.filterFilms}>Поиск</button>
                     </div>
+                </div>
+                <br/>
+                <div className="form-group">
+                    <h6 for="sel1" className="text text-primary">Жанры:</h6>
+                    <select ref="select" className="form-control" id="sel1" onChange={this.genreSearch}>
+                        {genres}
+                    </select>
                 </div>
             </aside>
         );
@@ -50,13 +88,14 @@ var Search = React.createClass({
 
 var App = React.createClass({
     getInitialState: function () {
-        return {films: []};
+        return {films: [], genres: []};
     },
 
     componentWillMount() {
         axios.get(`/all`)
             .then(res => {
-                this.setState({films: res.data});
+                console.log(res);
+                this.setState({films: res.data.films, genres: res.data.genres});
             });
     },
 
@@ -64,17 +103,25 @@ var App = React.createClass({
     filterFilms (name){
         axios.get(`/search/film?name=` + name)
             .then(res => {
-                this.setState({films: res.data});
+                this.setState({films: res.data, genres: this.state.genres});
             });
     },
 
-    autocomplete (q) {
+    search(q) {
         axios.get('/search/films', {params: {q: q}})
             .then(res => {
                 if (res.data.length == 0)
                     return;
 
-                this.setState({films: res.data});
+                this.setState({films: res.data, genres: this.state.genres});
+            });
+    },
+
+    filterByGenre(genre){
+        axios.get('/search/bygenre', {params: {genre: genre}})
+            .then(res => {
+                console.log(res);
+                this.setState({films: res.data, genres: this.state.genres});
             });
     },
 
@@ -89,7 +136,8 @@ var App = React.createClass({
             films = "По вашему запросу фильмы не найдеы";
         return (
             <div>
-                <Search filterFilms={this.filterFilms} autocomplete={this.autocomplete} key="search"/>
+                <Search filterByGenre={this.filterByGenre} filterFilms={this.filterFilms} search={this.search}
+                        genres={this.state.genres} key={films[0].id}/>
                 <article className="post content">
                     <ul className="post-list">
                         {films}
